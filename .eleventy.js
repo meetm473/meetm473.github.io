87947a0dc66a997d8cf1fbfcdf21bc74f73545e0
meetm473 <mehta.mayank860@gmail.com>
2025-01-02 23:11:27 +0530

    Added tool box content
    Attempted to add images for each tool, but could not find all of
    them in standard icon packages. Replacing the text of the tool
    with their svg image needs to be done later. Edit the svg code
    of downloaded icons.

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,40 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-
+const pluginIcons = require('eleventy-plugin-icons');
 
 module.exports = function(eleventyConfig) {
-
+  
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(pluginIcons, {
+    mode: 'inline',
+    sources: [
+              { name: 'simple', path: 'node_modules/simple-icons/icons', default: true},
+              { name: 'downloaded', path: './src/img/icons'}],
+    icon: {
+      shortcode: 'icon',
+      delimiter: ':',
+      transform: async (content) => content,
+      class: (name, source) => `icon icon-${name}`,
+      id: (name, source) => `icon-${name}`,
+      attributes: {},
+      attributesBySource: {},
+      overwriteExistingAttributes: true,
+      errorNotFound: true,
+    },
+    sprite: {
+      shortcode: 'spriteSheet',
+      attributes: {
+        class: 'sprite-sheet',
+        'aria-hidden': 'true',
+        xmlns: 'http://www.w3.org/2000/svg',
+      },
+      extraIcons: {
+        all: false,
+        sources: [],
+        icons: [],
+      },
+      writeFile: false,
+    },
+  });
   eleventyConfig.addPassthroughCopy('src/img')
 
 
